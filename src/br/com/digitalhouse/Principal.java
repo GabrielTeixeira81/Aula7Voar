e072043081add88f175003064092eb73dc46f7f4
GabrielTeixeira81 <gatecosvh>
2019-04-10 20:00:30 -0300

    Initial commit

---
+++ src/br/com/digitalhouse/Principal.java
@@ -0,0 +1,12 @@
+package br.com.digitalhouse;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        Aviao aviaoboing = new Aviao(4, "Asa curvada");
+        aviaoboing.voar();
+
+    }
+
+}
